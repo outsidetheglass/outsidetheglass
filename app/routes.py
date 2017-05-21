45f40b8a526f9ff9b2a87c5754b57da47a7ef893
Stevie Wanderer <outsidetheglass93@gmail.com>
2017-05-21 14:53:20 -0700

    testing merge conflicts

--- app/routes.py
+++ app/routes.py
@@ -5,7 +5,8 @@ from app import app
 from .models import *
 from .queries import *
 from .forms import *
-#stevie's comment
+#stevie's modified comment
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
